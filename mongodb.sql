95a574c1adbdba0611cbe8b4f555b268bb1ab40a
guruvardhan-tech-village <guruvardhaniniot@gmail.com>
2026-03-28 18:32:26 +0530

    new pro

---
+++ mongodb.sql
@@ -0,0 +1,22 @@
+ db.results.insertMany([
+    { id:1, eng:90, kannada:95, maths:78, science:88, social:91 },
+    { id:2, eng:88, kannada:85, maths:86, science:94, social:56 },
+    { id:3, eng:92, kannada:90, maths:80, science:85, social:89 },
+    { id:4, eng:85, kannada:88, maths:82, science:90, social:92 },
+    { id:5, eng:78, kannada:82, maths:88, science:91, social:87 },
+    { id:6, eng:91, kannada:94, maths:84, science:89, social:90 },
+    { id:7, eng:87, kannada:80, maths:90, science:86, social:88 },
+    { id:8, eng:89, kannada:92, maths:86, science:88, social:91 },
+    { id:9, eng:84, kannada:85, maths:82, science:90, social:89 },
+    { id:10, eng:90, kannada:88, maths:88, science:92, social:87 }
+])
+
+db.students.insertMany([
+    { id:4, name:"David", age:23, class:10 },
+    { id:5, name:"Eve", age:20, class:10 },
+    { id:6, name:"Frank", age:22, class:10 },
+    { id:7, name:"Grace", age:21, class:10 },
+    { id:8, name:"Heidi", age:23, class:10 },
+    { id:9, name:"Ivan", age:20, class:10 },
+    { id:10, name:"Judy", age:22, class:10 }
+])
